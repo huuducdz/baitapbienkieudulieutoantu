66ae7f0951247b4b02318de3810cece9db8d30ba
huuducdz <61704593+huuducdz@users.noreply.github.com>
2020-03-09 13:28:49 +0700

    Update baitap3.js

--- baitap3.js
+++ baitap3.js
@@ -1,5 +1,5 @@
 let bánkính;
 bánkính = prompt("Nhập bán kính hình tròn");
 const PI = 3.14 ;
-let diệntíchhìnhtròn = 2*PI*bánkính;
-document.write("diện tích hình tròn là :" + diệntíchhìnhtròn );
+let chuvihìnhtròn = 2*PI*bánkính;
+document.write("chu vi hình tròn là :" + chuvihìnhtròn );
